f84b1185ca924e263821f243db31527c5a9c43ab
D4LM <148903901+D4LMwastaken@users.noreply.github.com>
2024-10-06 20:12:28 -0400

    Fixed bug (try 4)

--- LoadPhotos.js
+++ LoadPhotos.js
@@ -1,18 +1,27 @@
-const folderPath = "photos/";
-
 function loadImages() {
-  const imageFolder = new File(folderPath);
-  const images = imageFolder.listFiles();
-
-  for (let i = 0; i < images.length; i++) {
-    const image = images[i];
-    if (image.isFile()) {
-      const img = document.createElement("img");
-      img.src = folderPath + image.getName();
-      img.className = "round animatedImg";
-      document.body.appendChild(img);
-    }
+    const folderPath = "photos/";
+  
+    const imageFolder = new XMLHttpRequest();
+    imageFolder.open("GET", folderPath, true);
+    imageFolder.responseType = "blob";
+  
+    imageFolder.onload = function() {
+      const reader = new FileReader();
+      reader.readAsDataURL(imageFolder.response);
+  
+      reader.onload = function() {
+        const images = reader.result.match(/<img src="([^"]+)">/g);
+  
+        if (images) {
+          images.forEach(image => {
+            const img = document.createElement("img");
+            img.src = image.match(/<img src="([^"]+)">/)[1];
+            img.className = "round animatedImg";
+            document.body.appendChild(img);
+          });
+        }
+      };
+    };
+  
+    imageFolder.send();
   }
-}
-
-loadImages();
